954520e200ae9a89cf54ac13063aa8034ba9e2ca
romanzemerov <romanzemerov@gmail.com>
2020-04-27 16:25:26 +0300

    move bin to src folder

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { userIdentification } from "./../src/cli.js";
+import userIdentification from "../cli.js";
 
 console.log("Welcome to the Brain Games!");
 userIdentification();
