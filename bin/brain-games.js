8a9eb3bd8b6e61874993ae1ea474df7090eac155
romanzemerov <romanzemerov@gmail.com>
2020-05-11 14:23:54 +0300

    fix eslint problems

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { userIdentification } from "../src/userIdentification.js";
+import { userIdentification } from "../src/user-identification.js";
 
 console.log("Welcome to the Brain Games!");
 userIdentification();
