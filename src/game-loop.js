0a5a1e87a3faf0a6663f2bb7df13555e1498b739
romanzemerov <romanzemerov@gmail.com>
2020-05-13 14:41:10 +0300

    refactoring game engine

--- src/game-loop.js
+++ src/game-loop.js
@@ -1,38 +1,24 @@
 import readlineSync from "readline-sync";
-import { getUserName } from "./user-identification.js";
 
-const ROUNDS = 3;
+const ROUNDS_COUNT = 3;
 
-const gameLoop = ({
-  generateQuestion,
-  processingUserAnswer,
-  getCorrectAnswer,
-}) => {
-  const getCongratulations = () => {
-    return `Congratulations, ${getUserName()}!`;
-  };
+const gameLoop = (getRound, getCongratulations, getErrorMessage) => {
+  for (let i = 0; i < ROUNDS_COUNT; i += 1) {
+    const { question, correctAnswer } = getRound();
 
-  const getErrorMessage = (correctAnswer, wrongAnswer) => {
-    return `${wrongAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${getUserName()}!`;
-  };
-
-  let finalMessage = getCongratulations();
-
-  for (let i = 0; i < ROUNDS; i += 1) {
-    console.log(`Question: ${generateQuestion()}`);
+    console.log(`Question: ${question}`);
 
     const answer = readlineSync.question("Your answer: ");
-    const isCorrectAnswer = processingUserAnswer(answer);
 
-    if (!isCorrectAnswer) {
-      finalMessage = getErrorMessage(getCorrectAnswer(), answer);
-      break;
+    if (answer !== correctAnswer) {
+      console.log(getErrorMessage(correctAnswer, answer));
+      return;
     }
 
     console.log("Correct!");
   }
 
-  console.log(finalMessage);
+  console.log(getCongratulations());
 };
 
 export default gameLoop;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import { userIdentification } from "./user-identification.js";
+import readlineSync from "readline-sync";
 import gameLoop from "./game-loop.js";
 
-const initGame = (game) => {
-  const { getRules } = game;
+const DEFAULT_NAME = "Anonymous";
 
-  userIdentification();
+const initGame = ({ rules, getRound }) => {
+  let name;
+
+  const getCongratulations = () => {
+    return `Congratulations, ${name}!`;
+  };
+
+  const getErrorMessage = (correctAnswer, wrongAnswer) => {
+    return `${wrongAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`;
+  };
+
+  const userInput = readlineSync.question("May I have your name? ");
+  name = userInput.length !== 0 ? userInput : DEFAULT_NAME;
+
+  console.log(`Hello, ${name}!`);
   console.log(`Welcome to the Brain Games!`);
-  console.log(getRules());
-  gameLoop(game);
+  console.log(rules);
+
+  gameLoop(getRound, getCongratulations, getErrorMessage);
 };
 
 export default initGame;
