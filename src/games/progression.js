949ed587a262e7db1a070b2c02437f5ac97f68ed
romanzemerov <romanzemerov@gmail.com>
2020-05-20 14:19:47 +0300

    refactor progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,25 +3,23 @@ import randomInteger from '../utils.js';
 const NUMBER_MIN = 0;
 const NUMBER_MAX = 100;
 const PROGRESSION_LENGTH = 10;
+const PROGRESSION_SHIFT_MIN = 0;
+const PROGRESSION_SHIFT_MAX = 10;
 
 const task = 'What number is missing in the progression?';
 
-const generateProgression = (step, length) => {
-  const PROGRESSION_SHIFT_MIN = 0;
-  const PROGRESSION_SHIFT_MAX = 10;
-
-  const newProgression = [];
-  const shift = randomInteger(PROGRESSION_SHIFT_MIN, PROGRESSION_SHIFT_MAX);
+const generateProgression = (step, length, shift = 0) => {
+  const progression = [];
 
   for (let i = 1; i <= length; i += 1) {
-    newProgression.push(i * step + shift);
+    progression.push(i * step + shift);
   }
 
-  return newProgression;
+  return progression;
 };
 
-const generateProgressionString = (array, hiddenIndex) => {
-  return array
+const getQuestion = (progression, hiddenIndex) => {
+  return progression
     .map((number, i) => (i === hiddenIndex ? '..' : number))
     .join(' ');
 };
@@ -29,8 +27,14 @@ const generateProgressionString = (array, hiddenIndex) => {
 const getRound = () => {
   const hiddenNumberIndex = randomInteger(0, PROGRESSION_LENGTH - 1);
   const progressionStep = randomInteger(NUMBER_MIN, NUMBER_MAX);
-  const progression = generateProgression(progressionStep, PROGRESSION_LENGTH);
-  const question = generateProgressionString(progression, hiddenNumberIndex);
+  const shift = randomInteger(PROGRESSION_SHIFT_MIN, PROGRESSION_SHIFT_MAX);
+  const progression = generateProgression(
+    progressionStep,
+    PROGRESSION_LENGTH,
+    shift,
+  );
+
+  const question = getQuestion(progression, hiddenNumberIndex);
   const correctAnswer = String(progression[hiddenNumberIndex]);
 
   return { question, correctAnswer };
